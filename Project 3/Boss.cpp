3a85cb444564c5ec5b29956dc73e9786798c1218
SquishyCode <mjmergal13@gmail.com>
2023-02-28 21:37:37 -0400

    Sprites boss 3 and 2 done

--- Project 3/Boss.cpp
+++ Project 3/Boss.cpp
@@ -2,7 +2,7 @@
 
 
 Boss::Boss() {
-	setPosition(df::Vector(60, 15));
+	setPosition(df::Vector(60, 15));					//Boss3 looks good in (70, 15) so move from out of screen to this pos.
 	setSprite("boss1");
 
 }
